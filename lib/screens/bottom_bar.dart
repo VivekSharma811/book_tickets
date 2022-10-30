ccba7eb6e65f09e21168b5a547d44e4352ec9b79
Vivek <vivek.sharma@mpokket.com>
2022-10-30 13:40:54 +0530

    SearchScreen completed

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -1,4 +1,5 @@
 import 'package:booktickets/screens/home_screen.dart';
+import 'package:booktickets/screens/search_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 
@@ -15,7 +16,7 @@ class _BottomBarState extends State<BottomBar> {
 
   static final List<Widget> _widgetOptions = <Widget>[
     HomeScreen(),
-    const Text("Search"),
+    SearchScreen(),
     const Text("Tickets"),
     const Text("Profile")
   ];
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:booktickets/screens/hotel_screen.dart';
 import 'package:booktickets/screens/ticket_view.dart';
 import 'package:booktickets/utils/app_info_list.dart';
 import 'package:booktickets/utils/app_styles.dart';
+import 'package:booktickets/widgets/view_all_widget.dart';
 import 'package:gap/gap.dart';
 import 'package:flutter/material.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
@@ -64,19 +65,8 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
                 const Gap(40),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Upcoming Flights", style: Styles.headLineStyle2),
-                    InkWell(
-                        onTap: () {
-
-                        },
-                        child: Text("View all",
-                            style: Styles.textStyle
-                                .copyWith(color: Styles.primaryColor)))
-                  ],
-                )
+                const ViewAll(
+                    bigText: "Upcoming flights", smallText: "View all")
               ],
             ),
           ),
@@ -85,32 +75,22 @@ class HomeScreen extends StatelessWidget {
             scrollDirection: Axis.horizontal,
             padding: const EdgeInsets.only(left: 20),
             child: Row(
-              children: ticketList.map((ticket) => TicketView(ticket: ticket)).toList(),
+              children: ticketList
+                  .map((ticket) => TicketView(ticket: ticket))
+                  .toList(),
             ),
           ),
           const Gap(15),
           Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Hotels", style: Styles.headLineStyle2),
-                InkWell(
-                    onTap: () {
-
-                    },
-                    child: Text("View all",
-                        style: Styles.textStyle
-                            .copyWith(color: Styles.primaryColor)))
-              ],
-            ),
-          ),
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: const ViewAll(bigText: "Hotels", smallText: "View all")),
           const Gap(15),
           SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             padding: const EdgeInsets.only(left: 20),
             child: Row(
-              children: hotelList.map((hotel) => HotelScreen(hotel: hotel)).toList(),
+              children:
+                  hotelList.map((hotel) => HotelScreen(hotel: hotel)).toList(),
             ),
           ),
         ],
